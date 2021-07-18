105e63c4356e263a105b19d4ead7b394781e950a
EP3IK <37956517+EP3IK@users.noreply.github.com>
2021-07-18 18:05:20 +0900

    Add files via upload

--- maplegame/puzzlemaster/index.js
+++ maplegame/puzzlemaster/index.js
@@ -1,4 +1,6 @@
-﻿const status = new Map();
+﻿const ext = document.createElement('canvas').toDataURL('image/webp').startsWith('data:image/webp') ? '.webp' : '.png';
+const info = document.querySelector('#info');
+const puzzle = new Map();
 
 const getImageFromUrl = async src => new Promise(resolve => {
   const image = new Image();
@@ -7,18 +9,20 @@ const getImageFromUrl = async src => new Promise(resolve => {
   image.src = src;
 });
 
-const getDataArray = async () => Promise.all(
-  Array.from({ length: 39 }, async (_, i) => {
-    const image = await getImageFromUrl(`${i}.png`);
-    const canvas = document.createElement('canvas');
-    canvas.width = 800;
-    canvas.height = 600;
-    const context = canvas.getContext('2d');
+const getDataArray = async () => {
+  const dataArray = [];
+  const canvas = document.createElement('canvas');
+  canvas.width = 800;
+  canvas.height = 600;
+  const context = canvas.getContext('2d');
+  for (let i = 0; i < 39; ++i) {
+    const image = await getImageFromUrl(i + ext);
     context.drawImage(image, 0, 0);
-    return context.getImageData(95, 55, 50, 50).data;
-    // return context.getImageData(1, 1, 105, 105).data;
-  })
-);
+    const { data } = context.getImageData(95, 55, 50, 50);
+    dataArray.push(data);
+  }
+  return dataArray;
+};
 
 const getImageFromClipboard = async () => {
   const items = await navigator?.clipboard?.read?.();
@@ -34,7 +38,6 @@ const getImageFromClipboard = async () => {
 };
 
 const getIndexFromCapture = (dataArray, image) => {
-  console.log(new Set(dataArray.map(v => v.join(','))).size)
   // sprite 이미지와 비교할 지점의 좌표를 찾고 데이터를 잘라낸다.
   const canvas = document.createElement('canvas');
   const context = canvas.getContext('2d'),
@@ -49,25 +52,26 @@ const getIndexFromCapture = (dataArray, image) => {
   });
   const sx = index % width + 97, sy = ~~(index / width) + 57;
   const cropData = context.getImageData(sx, sy, 50, 50).data;
-  console.log(cropData)
-  // dataArray와 비교해서 같은 데이터가 있는 인덱스를 리턴한다.
+  // dataArray에서 같은 데이터의 인덱스 값을 리턴한다.
   return dataArray.findIndex(data => data.every((v, i) => v === cropData[i]));
-  // return dataArray.findIndex(data => data === cropData);
 };
 
-const getRCDataArray = async index => {
-  const image = await getImageFromUrl(`${index}.png`);
+const getRCDataArray = () => {
+  const dataArray = [];
+  const image = puzzle.get('image');  // await getImageFromUrl(index + ext);
   const canvas = document.createElement('canvas');
   canvas.width = 800;
   canvas.height = 600;
   const context = canvas.getContext('2d');
   context.drawImage(image, 0, 0);
-  return Array.from({ length: 4 }, (_, r) => {
-    return Array.from({ length: 5 }, (_, c) => {
+  for (let r = 0; r < 4; ++r) {
+    for (let c = 0; c < 5; ++c) {
       const sx = 55 + 160 * c, sy = 65 + 150 * r;
-      return context.getImageData(sx, sy, 100, 30).data;
-    });
-  });
+      const { data } = context.getImageData(sx, sy, 100, 30);
+      dataArray.push(data);
+    }
+  }
+  return dataArray;
 };
 
 const getRCFromCapture = (dataArray, image) => {
@@ -84,63 +88,80 @@ const getRCFromCapture = (dataArray, image) => {
   });
   const sx = index % width + 56, sy = ~~(index / width) + 66;
   const cropData = context.getImageData(sx, sy, 100, 30).data;
-  console.log(sx, sy, cropData)
-  for (let r = 0; r < 4; ++r) {
-    for (let c = 0; c < 5; ++c) {
-      if (dataArray[r][c].every((v, i) => v === cropData[i])) return [r, c];
-    }
-  }
-  return [-1, -1];
+  const rcIndex = dataArray.findIndex(data => data.every((v, i) => v === cropData[i]));
+  return [~~(rcIndex / 5), rcIndex % 5];
 };
 
 const focusHandler = async () => {
-  if (status.has('index')) {
-    console.log('status has index');
+  if (puzzle.has('index')) {
     // 몇 번째 퍼즐인지 확인이 됐으면
-    const index = status.get('index');
-    // index.png 이미지를 가져오고 데이터를 만든다.
-    const base64Array = status.has('data') ? status.get('data') : await getRCDataArray(index);
-    // console.log(base64Array)
+    console.log('puzzle has index');
+    info.innerHTML = '퍼즐 조각의 위치 검색 시작';
+    // index.png 이미지에서 만들어진 데이터를 가져온다.
+    if (!puzzle.has('rc-data')) puzzle.set('rc-data', getRCDataArray());
+    const rcDataArray = puzzle.get('rc-data');
     // 클립보드 이미지와 데이터를 비교해서 rc 찾아낸다
     const image = await getImageFromClipboard();
-    if (!image) return;
-    const [row, col] = getRCFromCapture(base64Array, image);
+    if (!image) {
+      info.innerHTML = '클립보드 데이터에 이미지가 없습니다. 스크린샷을 찍어주세요.';
+      return;
+    }
+    info.innerHTML = '클립보드 이미지 확인. 퍼즐 조각의 위치 검색 중...';
+    const [row, col] = getRCFromCapture(rcDataArray, image);
     console.log(`row: ${row}, col: ${col}`);
-    if (row !== -1) {
-      status.set('row', row);
-      status.set('col', col);
-      // console.log(row, col)
+    if (col !== -1) {
+      info.innerHTML = `퍼즐 조각의 위치는 ${row + 1}행 ${col + 1}열입니다.`;
       document.querySelector('.selected')?.classList.remove('selected');
       [...document.querySelectorAll('#app > div')][5 * row + col].classList.add('selected');
       return;
     }
+    info.innerHTML = '클립보드 이미지에 오류가 있습니다. 스크린샷을 다시 찍어주세요.';
   } else {
-    console.log('status has no index');
     // 몇 번째 퍼즐인지 모르면
-    // sprite에서 데이터를 뽑아낸다.
-    const dataArray = await getDataArray();
+    console.log('puzzle has no index');
+    info.innerHTML = '퍼즐의 인덱스 검색 시작';
+    // 인덱스 데이터 행렬 가져온다.
+    if (!puzzle.has('index-data')) puzzle.set('index-data', await getDataArray());
+    const dataArray = puzzle.get('index-data');
     // 클립보드에서 이미지 가져오고 비교해서 index 찾아낸다.
     const image = await getImageFromClipboard();
-    if (!image) return;
+    if (!image) {
+      info.innerHTML = '클립보드 데이터에 이미지가 없습니다. 스크린샷을 찍어주세요.';
+      return;
+    }
+    info.innerHTML = '클립보드 이미지 확인. 퍼즐의 인덱스 검색 중...';
     const index = getIndexFromCapture(dataArray, image);
-    if (index < 0) return;
-    // status에 index 등록한다.
-    status.set('index', index);
+    if (index < 0) {
+      info.innerHTML = '클립보드 이미지에 오류가 있습니다. 스크린샷을 다시 찍어주세요.';
+      return;
+    }
+    info.innerHTML = `퍼즐의 인덱스는 ${index} / 38 입니다.`;
+    // puzzle에 index 등록한다.
+    puzzle.set('index', index);
+    puzzle.set('image', await getImageFromUrl(index + ext));
+    puzzle.set('rc-data', getRCDataArray(index + ext));
     // 배경에 index.png 깔아놓는다.
-    // const root = document.querySelector(':root');
-    // root.style.setProperty('--src', `url(${index}.png)`);
-    document.querySelector('#app').style.background = `url(${index}.png) 0 0`;
+    document.querySelector('#app').style.background = `url(${index}${ext}) 0 0`;
     document.querySelector('.selected')?.classList.remove('selected');
   }
 };
 
+const refresh = () => {
+  for (const key of ['index', 'image', 'rc-data']) {
+    puzzle.delete(key);
+  }
+  focusHandler();
+};
+
+window.addEventListener('load', async () => {
+  console.log('window loaded');
+  puzzle.set('index-data', await getDataArray());
+  document.querySelector('#app').append(...Array.from({ length: 20 }, () => document.createElement('div')));
+  navigator?.clipboard?.read?.();
+  focusHandler();
+});
 window.addEventListener('focus', () => {
   console.log('window focused');
   focusHandler();
-})
-document.querySelector('#app').append(...Array(20).fill(0).map(_ => document.createElement('div')));
-for (let i = 0; i < 39; ++i) {
-  // preload
-  getImageFromUrl(`${i}.png`);
-}
-navigator?.clipboard?.read?.();
+});
+document.querySelector('#refresh').addEventListener('click', refresh);
